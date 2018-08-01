b3c3141eed88005266a288822dcf12ced2d94aa1
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-08-01 09:53:59 +0800

    Adding Preview Window boilerplate

---
+++ src/components/layout/PreviewWindow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PreviewWindow extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PreviewWindow;
