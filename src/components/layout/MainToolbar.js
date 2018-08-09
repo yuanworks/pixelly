72254c70f4b1def18bfa6ae79a731761d72c94d1
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-08-09 15:40:08 +0800

    rc-slider styling

--- src/components/layout/MainToolbar.js
+++ src/components/layout/MainToolbar.js
@@ -3,11 +3,17 @@ import './MainToolbar.css';
 import Slider from 'rc-slider/lib/Slider';
 import 'rc-slider/assets/index.css';
 
+function log(value) {
+  console.log(value);
+}
+
 class MainToolbar extends Component {
   render() {
     return (
       <div className="MainToolbar">
-        <Slider min={1} />
+        <div style={{width: 100}}>
+          <Slider min={1} max={20} defaultValue={1} onAfterChange={log} handleStyle={{backgroundColor: '#404040', borderColor: '#e9e9e9'}} dotStyle={{color: '#fff'}} />
+        </div>
       </div>
     )
   }
