732e96ab4fea820989a7ead0ab5996bb24b1c5de
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-08-09 08:14:48 +0800

    Redux: Adding brush reducer

---
+++ src/reducers/brushSelect.js
@@ -0,0 +1,13 @@
+import { Brushes } from '../actions';
+
+const selectedBrush = (state = Brushes.PENCIL, action) => {
+  switch (action.type) {
+    case 'SET_SELECTED_BRUSH':
+      return action.brush;
+    
+    default:
+      return state;
+  }
+}
+
+export default selectedBrush;
