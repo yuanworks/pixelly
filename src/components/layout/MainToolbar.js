091674daf6ed9d5d09b0f3c6a204965973aaf2e6
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-08-10 15:42:51 +0800

    Toolbar Input component

--- src/components/layout/MainToolbar.js
+++ src/components/layout/MainToolbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './MainToolbar.css';
 import Slider from 'rc-slider/lib/Slider';
 import 'rc-slider/assets/index.css';
+import ToolbarInput from './ToolbarInput';
 
 function log(value) {
   console.log(value);
@@ -12,7 +13,7 @@ class MainToolbar extends Component {
     return (
       <div className="MainToolbar">
         <div style={{display: "inline-block"}}>Border:</div>
-        <input className="MainToolbar-input" type="text" name="border-width" />
+        <ToolbarInput />
         <div className="MainToolbar-slider">
           <Slider min={1} max={20} defaultValue={1} onAfterChange={log} railStyle={{backgroundColor: '#948e8e'}} handleStyle={{backgroundColor: '#e9e9e9', borderColor: '#e9e9e9'}} dotStyle={{color: '#fff'}} />
         </div>
---
+++ src/components/layout/ToolbarInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ToolbarInput.css';
+
+class ToolbarInput extends Component {
+  render() {
+    return (
+      <input className="ToolbarInput" type="text" value={this.props.value} />
+    )
+  }
+}
+
+export default ToolbarInput;
